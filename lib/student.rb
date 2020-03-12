5ce378b193ff9c807dabb9989a2910a51821caee
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 15:30:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,22 @@ class Student
     @grade = grade
   end
   
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
   
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+      
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
