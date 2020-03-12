ec5b3a5027c4295ef6f1164c42a61a7eea429d82
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 15:47:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,10 @@ class Student
   end
   
   def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end
+  
     
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
