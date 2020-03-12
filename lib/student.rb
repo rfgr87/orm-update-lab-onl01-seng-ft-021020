75560c5b39eeec1e04ab5c24099987f93f58b932
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 15:45:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,8 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM songs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Song.new(result[0], result[1], result[2])
+    self.new_from_db(result)
+    //Song.new(result[0], result[1], result[2])
   end
   
   
